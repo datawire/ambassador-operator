81e40830ccc1bca0fce63350b4f741a9602b9abd
Bruce Horn <bruce@datawire.io>
2020-05-25 08:40:12 -0700

    fixed logging error

--- pkg/controller/ambassadorinstallation/reconcile.go
+++ pkg/controller/ambassadorinstallation/reconcile.go
@@ -122,7 +122,7 @@ func (r *ReconcileAmbassadorInstallation) Reconcile(request reconcile.Request) (
 	ambIns, err := r.lookupAmbInst(ambInstName)
 
 	if err != nil {
-		reqLogger.Info("Failed to lookup resource")
+		log.Info("Failed to lookup resource")
 		return reconcile.Result{}, err
 	}
 
