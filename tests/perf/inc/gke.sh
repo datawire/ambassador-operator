9469013c87b1a5e26cfd58c9af3fa5409c9d9344
Alvaro Saurin <alvaro.saurin@gmail.com>
2020-07-17 10:24:36 +0200

    Remove perf-tests
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- tests/perf/inc/gke.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# define some default values for GKE (if they have not been defined)
-
-export CLUSTER_SIZE="${CLUSTER_SIZE:-3}"
-
-# see https://cloud.google.com/compute/docs/machine-types?hl=en
-export CLUSTER_MACHINE="${CLUSTER_MACHINE:-}"
